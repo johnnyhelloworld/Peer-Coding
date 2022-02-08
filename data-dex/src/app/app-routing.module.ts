39f97260174d250d8cf2cb75382eeb5b2ae1a16c
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2022-02-08 14:40:19 +0100

    added fonctional navbar

--- data-dex/src/app/app-routing.module.ts
+++ data-dex/src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MyPokemonComponent } from './my-pokemon/my-pokemon.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
 const routes: Routes = [
   {
     path: "",
-    component: PokemonListComponent,
+    component: NavbarComponent,
     children: [
       {
-    path: "my-pokemon",
-    component: MyPokemonComponent,
-    }]
+        path: "my-pokemon",
+        component: MyPokemonComponent,
+      },
+      {
+        path: "pokedex",
+        component: PokemonListComponent,
+      },
+    ]
   }
 ];
 
